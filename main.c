35f2fcc748eb136e2c67942713a181d00cbaac39
SantiagoBenitez1999 <benitezsantiago081@gmail.com>
2020-10-25 19:31:20 -0300

    Agrego GIC del Parser y posible TT del Scanner

--- main.c
+++ main.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+    IDENTIFICADOR, PALABRARESERVADA, OPERADOR, CONSTANTE, CADENA
+} TOKEN;
+
+TOKEN scanner(void);
 
 int main()
 {
-    char caracterLeido;
+    char proximoCaracter;
+    char programaFuente[500];
+    int cont = 0;
     FILE *ptr = fopen("Programa fuente.txt", "r");
 
     if (!ptr){
         printf("ERROR: No se pudo abrir el archivo");
         exit(1);
     }
-    while(fscanf(ptr,"%c", &caracterLeido) != EOF){
-        printf("%c", caracterLeido);
+    while(fscanf(ptr,"%c", &proximoCaracter) != EOF){
+        programaFuente[cont] = proximoCaracter;
+        cont ++;
     }
     fclose(ptr);
+}
+
+TOKEN scanner(){
 }
